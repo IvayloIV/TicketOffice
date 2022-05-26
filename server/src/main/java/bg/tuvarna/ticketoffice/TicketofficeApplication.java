6d15dab66563cb2bf2e560190d1e557debdf6250
ivaylo_nikolaev <ivaylo_nikolaev@stemo.bg>
2022-05-26 23:50:59 +0300

    Init server project

---
+++ server/src/main/java/bg/tuvarna/ticketoffice/TicketofficeApplication.java
@@ -0,0 +1,13 @@
+package bg.tuvarna.ticketoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketofficeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketofficeApplication.class, args);
+    }
+
+}
